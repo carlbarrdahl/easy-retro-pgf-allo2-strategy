000ecdff9b1f7bc6e062b401ea40b9d15bfec026
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-03-25 10:40:09 +0100

    Print network name when deploying

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,7 +2,7 @@ import hre from "hardhat";
 
 async function main() {
   const alloAddress = "0x1133eA7Af70876e64665ecD07C0A0476d09465a1";
-  console.log("Deploying contract...");
+  console.log(`Deploying contract to ${hre.network.name}...`);
   const strategy = await hre.viem.deployContract("EasyRPGFStrategy", [
     alloAddress,
     "EasyRPGFStrategy",
