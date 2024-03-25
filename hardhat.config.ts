cd67223d5397638f7bda90b74a90f956ce34cbf1
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-03-25 16:27:21 +0100

    Add Optimism etherscan key

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -34,6 +34,7 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: {
+      optimisticEthereum: ETHERSCAN_KEY,
       optimismSepolia: ETHERSCAN_KEY,
       celo: CELO_ETHERSCAN_KEY,
     },
